6dc591a833b3e7ee1a6947f8907e799415d1cf4d
rethacindyaulia <rethacindyauliaa@gmail.com>
2024-11-30 02:05:56 +0700

    tugas2

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.centerText}>RETHA CINDY AULIA</Text>
+      <View style={styles.flexContainer}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#d3d3d3', // Latar belakang abu-abu terang
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  centerText: {
+    fontSize: 24, // Ukuran font 24
+    color: 'blue', // Warna biru
+    fontWeight: 'bold', // Gaya teks tebal
+    marginBottom: 20
+  },
+  flexContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%'
+  },
+  box1: {
+    backgroundColor: '#F08080', // Warna bebas untuk kotak 1
+    width: 100,
+    height: 100
+  },
+  box2: {
+    backgroundColor: '#DB7093', // Warna bebas untuk kotak 2
+    width: 100,
+    height: 100
+  }
 });
+
+export default App;
